8978e2f684ac2fb9fb7976d813968a841859aad2
RadostW <radost.waszkiewicz@gmail.com>
2024-01-27 11:24:51 +0100

    More templates

--- collision_kernels/generate_trajectories.py
+++ collision_kernels/generate_trajectories.py
@@ -1,1 +1,30 @@
-.
+def generate_trajectories(peclet=1, small_r=0.05, trials=1000, floor_r=10, floor_h=10):
+    """
+    Generate trajectories of particles in a simulation.
+
+    Parameters
+    ----------
+    peclet : float, optional
+        Peclet number defined as R u / D.        
+
+    small_r : float, optional
+        Radius of the smal ball.        
+
+    trials : int, optional
+        Number of trajectories.
+
+    floor_r : int, optional
+        Radius of the floor of trial cyllinder.
+
+    floor_h : int, optional
+        Vertical distance from initial condition to big ball centre.
+
+    Returns
+    -------
+    np.array
+        2 by `trials` array containing initial radial distance in 
+        first coord and outcome 1 - ball hit, 0 - ball missed.
+
+    """
+
+    pass
--- collision_kernels/process_trajectories.py
+++ collision_kernels/process_trajectories.py
@@ -1,1 +1,17 @@
-.
+def effective_radius(trajectory_array):
+    """
+    Compute effective area of the particle from collision data
+
+    Parameters
+    ----------
+    trajectory_array : np.array
+        2 by N array of initial radius to outcome (1 - hit, 0 - miss)
+
+    Returns
+    -------
+    float
+        Effective radius of the particle
+
+    """
+
+    pass
